a790afea5f878554653ffa2d96092d4ffb2b645e
Sunil Paul <sunilpaul16@gmail.com>
2024-07-24 14:32:53 +0100

    add Link

--- MyNewApp/app/index.tsx
+++ MyNewApp/app/index.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My-App!!</Text>
       <StatusBar style="auto" />
+      <Link href="/login">login</Link>
     </View>
   );
 }
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
