8eb32e726c53abd977353f2a51bbf6819cfc2803
Sunil Paul <sunilpaul16@gmail.com>
2024-07-24 15:47:03 +0100

    nativeWind add

--- MyNewApp/app/index.tsx
+++ MyNewApp/app/index.tsx
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My-App!!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl">My-App!!</Text>
       <StatusBar style="auto" />
       <Link href="/login">login</Link>
+      <Link href="/signup">signUp</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
--- MyNewApp/app/signup.tsx
+++ MyNewApp/app/signup.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const signUp = () => {
   return (
     <View>
-      <Text>Sign up</Text>
+      <Text>Sign Up</Text>
     </View>
-  )
-}
+  );
+};
 
-export default signUp
+export default signUp;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
