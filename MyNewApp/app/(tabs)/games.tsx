27939c810feba50450e9bf1d8adc0795d6b12a42
Sunil Paul <sunilpaul16@gmail.com>
2024-07-24 20:01:30 +0100

    clean-up

--- MyNewApp/app/(tabs)/games.tsx
+++ MyNewApp/app/(tabs)/games.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
 
 const Games = () => {
@@ -11,4 +11,3 @@ const Games = () => {
 
 export default Games;
 
-const styles = StyleSheet.create({});
--- MyNewApp/app/(tabs)/movies.tsx
+++ MyNewApp/app/(tabs)/movies.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
 
 const Movies = () => {
@@ -10,5 +10,3 @@ const Movies = () => {
 };
 
 export default Movies;
-
-const styles = StyleSheet.create({});
