19a88a1e928e25307bd60e4e944374099e543b21
Sunil Paul <sunilpaul16@gmail.com>
2024-07-26 21:12:32 +0100

    changed file to TS

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import router from './router';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
