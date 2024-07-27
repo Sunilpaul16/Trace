6a29c42399c3eb2553c1a8b02e1d4e4d347ce594
Sunil Paul <sunilpaul16@gmail.com>
2024-07-27 14:04:51 +0100

    added routes for games and books

--- server/router.ts
+++ server/router.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
-import { getMovies, postMovie } from './controllers';
+import {
+  getBooks,
+  getGames,
+  getMovies,
+  postGame,
+  postMovie
+} from './controllers';
 
 const router = Router();
 
 router.get('/movies', getMovies);
 router.post('/movies', postMovie);
+
+router.get('/books', getBooks);
+router.post('/books', getBooks);
+
+router.get('/games', getGames);
+router.post('/games', postGame);
 
 export default router;
 router;
