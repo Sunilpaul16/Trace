56302e9f25660fdf020574ad363fb45c1e3627ce
Sunil Paul <sunilpaul16@gmail.com>
2024-07-28 01:43:49 +0100

    add error handling for existing data

--- server/controllers.ts
+++ server/controllers.ts
@@ -17,7 +17,7 @@ export const postMovie = async (req: Request, res: Response) => {
     const existingMovie = await Movie.findOne({ id: id });
 
     if (existingMovie) {
-      return res.json({ message: 'Movie already exists in database' });
+      return res.json({ 'Movie already exists in database': existingMovie });
     }
     const newMovie = new Movie(req.body);
     await newMovie.save();
@@ -41,24 +41,25 @@ export const postBook = async (req: Request, res: Response) => {
 
   try {
     const existingBook = await Book.findOne({ id: id });
-
     if (existingBook) {
-      return res.json({ message: 'Book already exists in database' });
+      return res
+        .status(200)
+        .json({ 'Book already exists in database': existingBook });
     }
     const newBook = new Book(req.body);
     await newBook.save();
-    res.status(201).send(newBook);
+    res.status(201).json(newBook);
   } catch (error) {
-    res.status(500).send('Error while posting a Book');
+    res.status(500).json({ 'Error while posting a Book': error });
   }
 };
 
 export const getGames = async (req: Request, res: Response) => {
   try {
-    const movies = await Movie.find();
-    res.status(200).send(movies);
+    const games = await Game.find();
+    res.status(200).json(games);
   } catch (error) {
-    res.status(400).send('Error while getting movies');
+    res.status(400).json({ 'Error while getting games': error });
   }
 };
 
@@ -69,12 +70,13 @@ export const postGame = async (req: Request, res: Response) => {
     const existingGame = await Game.findOne({ id: id });
 
     if (existingGame) {
-      return res.json({ message: 'Game already exists in database' });
+      return res.json({ 'Game already exists in database': existingGame });
     }
+
     const newGame = new Game(req.body);
     await newGame.save();
-    res.status(201).send(newGame);
+    res.status(201).json(newGame);
   } catch (error) {
-    res.status(500).send('Error while posting a Game');
+    res.status(500).json({ 'Error while posting a Game': error });
   }
 };
