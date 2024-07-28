55cfe5b616387a6e574338cc2647bfd92f129894
Sunil Paul <sunilpaul16@gmail.com>
2024-07-28 14:39:17 +0100

    Refactor router

--- server/router.ts
+++ server/router.ts
@@ -1,23 +1,29 @@
 import { Router } from 'express';
 import {
-  getBooks,
-  getGames,
+  deleteMovie,
   getMovies,
-  postBook,
-  postGame,
   postMovie
-} from './controllers';
+} from './controllers/movies-controllers';
+import {
+  getBooks,
+  postBook,
+  deleteBook
+} from './controllers/books-controllers';
+import { getGames, postGame, deleteGame } from './controllers/games-controller';
 
 const router = Router();
 
 router.get('/movies', getMovies);
 router.post('/movies', postMovie);
+router.delete('/movies/:id', deleteMovie);
 
 router.get('/books', getBooks);
 router.post('/books', postBook);
+router.delete('/books/:id', deleteBook);
 
 router.get('/games', getGames);
 router.post('/games', postGame);
+router.delete('/games/:id', deleteGame);
 
 export default router;
 router;
