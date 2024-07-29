7066095435f97b7c75827f79f87e295a20c10cba
Sunil Paul <sunilpaul16@gmail.com>
2024-07-29 01:10:36 +0100

    Refactor component imports and update icons usage

--- MyNewApp/assets/icons.tsx
+++ MyNewApp/assets/icons.tsx
@@ -1,6 +1,7 @@
 import {
   AntDesign,
   Entypo,
+  FontAwesome5,
   Ionicons,
   MaterialCommunityIcons
 } from '@expo/vector-icons';
@@ -18,6 +19,32 @@ const movieIcon = (
 );
 const searchIcon = <AntDesign name="search1" size={24} color="white" />;
 
+const steamIcon = <FontAwesome5 name="steam" size={24} color="black" />;
+const epicIcon = <FontAwesome5 name="epic-games" size={24} color="black" />;
+const uplayIcon = <FontAwesome5 name="uplay" size={24} color="black" />;
+const originIcon = <FontAwesome5 name="origin" size={24} color="black" />;
+const battlenetIcon = (
+  <FontAwesome5 name="battle-net" size={24} color="black" />
+);
+const gogIcon = <FontAwesome5 name="gog" size={24} color="black" />;
+const psIcon = <FontAwesome5 name="playstation" size={24} color="black" />;
+const xboxIcon = <FontAwesome5 name="xbox" size={24} color="black" />;
+const switchIcon = (
+  <FontAwesome5 name="nintendo-switch" size={24} color="black" />
+);
+const androidIcon = <FontAwesome5 name="android" size={24} color="black" />;
+const appleIcon = <FontAwesome5 name="apple" size={24} color="black" />;
+const windowsIcon = <FontAwesome5 name="windows" size={24} color="black" />;
+const linuxIcon = <FontAwesome5 name="linux" size={24} color="black" />;
+const macIcon = <FontAwesome5 name="apple" size={24} color="black" />;
+
+const plusIcon = <AntDesign name="plus" size={24} color="black" />;
+const minusIcon = <AntDesign name="minus" size={24} color="black" />;
+const closeIcon = <AntDesign name="close" size={24} color="black" />;
+const checkIcon = <AntDesign name="check" size={24} color="black" />;
+const arrowLeftIcon = <AntDesign name="arrowleft" size={24} color="black" />;
+const arrowRightIcon = <AntDesign name="arrowright" size={24} color="black" />;
+
 export {
   eyeShowIcon,
   eyeHideIcon,
@@ -25,5 +52,19 @@ export {
   bookIcon,
   gameIcon,
   movieIcon,
-  searchIcon
+  searchIcon,
+  steamIcon,
+  epicIcon,
+  uplayIcon,
+  originIcon,
+  battlenetIcon,
+  gogIcon,
+  psIcon,
+  xboxIcon,
+  switchIcon,
+  androidIcon,
+  appleIcon,
+  windowsIcon,
+  linuxIcon,
+  macIcon
 };
