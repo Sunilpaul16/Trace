a9eb6da30dd68b1b96aba225df59d76d47faaa78
Sunil Paul <sunilpaul16@gmail.com>
2025-06-24 06:13:48 +0100

    Refactor router.ts formatting

--- server/router.ts
+++ server/router.ts
@@ -1,29 +1,28 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   deleteMovie,
   getMovies,
-  postMovie
-} from './controllers/movies-controllers';
+  postMovie,
+} from "./controllers/movies-controllers";
 import {
   getBooks,
   postBook,
-  deleteBook
-} from './controllers/books-controllers';
-import { getGames, postGame, deleteGame } from './controllers/games-controller';
+  deleteBook,
+} from "./controllers/books-controllers";
+import { getGames, postGame, deleteGame } from "./controllers/games-controller";
 
 const router = Router();
 
-router.get('/movies', getMovies);
-router.post('/movies', postMovie);
-router.delete('/movies/:id', deleteMovie);
+router.get("/movies", getMovies);
+router.post("/movies", postMovie);
+router.delete("/movies/:id", deleteMovie);
 
-router.get('/books', getBooks);
-router.post('/books', postBook);
-router.delete('/books/:id', deleteBook);
+router.get("/books", getBooks);
+router.post("/books", postBook);
+router.delete("/books/:id", deleteBook);
 
-router.get('/games', getGames);
-router.post('/games', postGame);
-router.delete('/games/:id', deleteGame);
+router.get("/games", getGames);
+router.post("/games", postGame);
+router.delete("/games/:id", deleteGame);
 
 export default router;
-router;
